da6c6a7231e4b7c5c0bac08a89e5a900ac487f74
shoheiyokoyama <shohei.yok0602@gmail.com>
2016-02-20 23:38:18 +0900

    create playground

---
+++ Swift-Tour.playground/Contents.swift
@@ -0,0 +1,19 @@
+//: Playground - noun: a place where people can play
+
+import UIKit
+
+print("Hello, Swift Tour")
+var val = 10
+val = 20
+let staticVal = 15
+//staticVal = 25 //Error
+
+let label = "The width is "
+let width = 75
+let widthLabel = label + String(width)
+
+let apples = 3
+let oranges = 5
+let appleSummary = "I have \(apples) apples."
+let fruitSummary = "I have \(apples + oranges) pieces of fruit"
+
