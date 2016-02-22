373f82bac2802c13369437f2be3832cc6f4c885f
shoheiyokoyama <shohei.yok0602@gmail.com>
2016-02-22 23:35:15 +0900

    practice enum

--- Swift-Tour.playground/Contents.swift
+++ Swift-Tour.playground/Contents.swift
@@ -317,6 +317,50 @@ print(triangleAndSquare.triangle.sideLength)
 
 
 
+enum Rank: Int {
+    case Ace = 1
+    case Two, Three, Four, Five, Six, Seven, Eight, Nine, Ten
+    case Jack, Queen, King
+    func simpleDescription() -> String {
+        switch self {
+        case .Ace:
+            return "ace"
+        case .Jack:
+            return "jack"
+        case .Queen:
+            return "king"
+        default:
+            return String(self.rawValue)
+        }
+    }
+}
+
+let ace = Rank.Ace
+let aceRawValue = ace.rawValue
+
+if let corvertedRank = Rank(rawValue: 3) {
+    let threeDescription = corvertedRank.simpleDescription()
+}
+
+enum Suit {
+    case Spades, Hearts, Diamonds, Clubs
+    func simpleDescription() -> String {
+        switch self {
+        case .Spades:
+            return "spades"
+        case .Hearts:
+            return "hearts"
+        case .Diamonds:
+            return "diamonds"
+        case .Clubs:
+            return "clubs"
+        }
+    }
+}
+
+let hearts = Suit.Hearts
+let heartsDescription = hearts.simpleDescription()
+
 
 
 
