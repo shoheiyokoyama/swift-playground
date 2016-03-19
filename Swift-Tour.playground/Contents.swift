22a7e654c0c9efdaf6fb90afbdb51dab4d1286c2
shoheiyokoyama <shohei.yok0602@gmail.com>
2016-03-19 18:41:08 +0900

    test protocol

--- Swift-Tour.playground/Contents.swift
+++ Swift-Tour.playground/Contents.swift
@@ -389,10 +389,47 @@ case let .Error(error):
     print("Failure... \(error)")
 }
 
+protocol ExampleProtocol {
+    var simpleDescription: String {get}
+    mutating func adjust()
+}
 
+class SimpleClass: ExampleProtocol {
+    var simpleDescription: String = "A very simple class"
+    var anotherProperty: Int = 692929
+    func adjust() {
+        simpleDescription += "Now 100% adjusted."
+    }
+}
 
+var a = SimpleClass()
+a.adjust()
+let aDescription = a.simpleDescription
 
+struct SimpleStructure: ExampleProtocol {
+    var simpleDescription: String = "A simple structure"
+    mutating func adjust() {
+        simpleDescription += " (adjusted)"
+    }
+}
 
+var b = SimpleStructure()
+b.adjust()
+let bDescription = b.simpleDescription
+
+extension Int: ExampleProtocol {
+    var simpleDescription: String {
+        return "The number \(self)"
+    }
+    mutating func adjust() {
+        self += 40
+    }
+}
+
+print(7.simpleDescription)
+
+let protocolValue: ExampleProtocol = a
+print(protocolValue.simpleDescription)
 
 
 
